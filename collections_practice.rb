5f0aac46931938efb301dbb56f83dee4aa93b4eb
Lydell Gaines <dasnic70@gmail.com>
2020-02-21 03:44:12 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,6 +8,11 @@ def sort_array_desc (array)
   array.sort {|x,y| y <=> x }
 end
 
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.length <=> right.length
+  end
+end
 
 
 def swap_elements (array)
